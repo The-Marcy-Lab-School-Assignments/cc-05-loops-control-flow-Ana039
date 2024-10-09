16d3482af3db5646bed41f54f5d5822926038fb8
Anayely <anayelialmonte@gmail.com>
2024-10-09 11:01:37 -0400

    up to number three

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+ 
+    
+
+
+const greaterNum = (num1, num2) => {
+if (Number.Interger(num1) && Number.Interger(num2)) {
+    if (num1 === num2) {
+        return "both intergers are equal";
+     }
+    else if (num1 > num2) {
+        return num1;
+     } else {
+        return num2;
+     } 
+    } else {
+       return null;
+     }
+    };
 
